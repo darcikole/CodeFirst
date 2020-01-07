1f384ee445cecb017f5721dd074eb90a2d9b1966
Darci Kole <darcikole@me.com>
2020-01-07 11:50:41 -0800

    Initial commit

---
+++ Models/EmployeeDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models
+{
+    public class EmployeeDetails
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int? Age { get; set; }
+        public decimal? Salary { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ Models/EmployeeDetailsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models
+{
+    public class EmployeeDetailsContext : DbContext
+    {
+        //public EmployeeDetailsContext() : base("DefaultConnection")
+        //{
+
+        //}
+        public DbSet<EmployeeDetails> EmployeeDetails { get; set; }
+
+    } 
+}
